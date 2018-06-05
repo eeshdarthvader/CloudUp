cc26e6f4dcb92b2614d579b9fe83e1024ec77eac
eesh.tyagi@gmail.com <eesh.tyagi@gmail.com>
2018-06-05 07:59:29 +0530

    Styling fixes and added file names to be shown after uploading

--- app/utils/cloudinaryAccount.js
+++ app/utils/cloudinaryAccount.js
@@ -1,6 +1,6 @@
 const CloudinaryConfig = {
-  upload_preset: 'w0htfe5t',
-  api_key: '948861642597617'
-}
+	upload_preset: '',
+	api_key: ''
+};
 
-export default CloudinaryConfig
+export default CloudinaryConfig;
