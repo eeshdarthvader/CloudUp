f7fad60afbe640e2b6bf7083bd98828a3b94d2e6
eesh.tyagi@gmail.com <eesh.tyagi@gmail.com>
2018-06-09 09:27:31 +0530

    Options to give upload preset and api key in app

--- app/components/Cloudinary.js
+++ app/components/Cloudinary.js
@@ -3,7 +3,7 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import Dropzone from 'react-dropzone';
 import axios from 'axios';
-import { Alert } from 'reactstrap';
+import { Alert, Input } from 'reactstrap';
 import { Line } from 'rc-progress';
 import CloudinaryIcon from './CloudinaryIcon';
 import CloudinaryConfig from '../utils/cloudinaryAccount';
@@ -14,7 +14,15 @@ type Props = {};
 export default class Cloudinary extends Component<Props> {
 	constructor() {
 		super();
-		this.state = { files: [], percent: 0, color: '#3FC7FA', error: '' };
+		this.state = {
+			files: [],
+			percent: 0,
+			color: '#3FC7FA',
+			error: '',
+			upload_preset: '',
+			api_key: '',
+			folderPath: ''
+		};
 	}
 
 	props: Props;
@@ -28,10 +36,13 @@ export default class Cloudinary extends Component<Props> {
 		const uploaders = files.map((file) => {
 			// Initial FormData
 			const formData = new FormData();
+			const uploadPreset = context.state.upload_preset || CloudinaryConfig.upload_preset;
+			const apiKey = context.state.api_key || CloudinaryConfig.api_key;
 			formData.append('file', file);
 			formData.append('tags', `cleartrip, medium, gist`);
-			formData.append('upload_preset', CloudinaryConfig.upload_preset); // Replace the preset name with your own
-			formData.append('api_key', CloudinaryConfig.api_key); // Replace API key with your own Cloudinary key
+			formData.append('folder', context.state.folderPath);
+			formData.append('upload_preset', uploadPreset); // Replace the preset name with your own
+			formData.append('api_key', apiKey); // Replace API key with your own Cloudinary key
 			formData.append('timestamp', (Date.now() / 1000) | 0);
 
 			const config = {
@@ -61,10 +72,17 @@ export default class Cloudinary extends Component<Props> {
 					const data = response.data;
 					const fileURL = data.secure_url; // You should store this URL for future references in your app
 					console.log(data);
+
 					this.setState({
 						files,
 						percent: 0
 					});
+
+					setTimeout(() => {
+						context.setState({
+							files: []
+						});
+					}, 5000);
 				})
 				.catch(function(error) {
 					console.log(error.response.data.error);
@@ -83,6 +101,18 @@ export default class Cloudinary extends Component<Props> {
 		axios.all(uploaders).then(() => {
 			// ... perform after upload is successful operation
 		});
+	};
+
+	handleUploadPresetChange = (e) => {
+		this.setState({ upload_preset: e.target.value });
+	};
+
+	handleApiKeyChange = (e) => {
+		this.setState({ api_key: e.target.value });
+	};
+
+	handleFolderPathChange = (e) => {
+		this.setState({ folderPath: e.target.value });
 	};
 
 	render() {
@@ -107,6 +137,15 @@ export default class Cloudinary extends Component<Props> {
 				{this.state.error && (
 					<Alert color="danger" className={styles.errorMsg}>
 						{this.state.error}
+					</Alert>
+				)}
+				{this.state.files.length !== 0 && (
+					<Alert color="danger" className={styles.errorMsg}>
+						{this.state.files.map((f) => (
+							<li key={f.name}>
+								{f.name} - {f.size / 1000} KB
+							</li>
+						))}
 					</Alert>
 				)}
 				<div className={styles.container} data-tid="container">
@@ -138,17 +177,29 @@ export default class Cloudinary extends Component<Props> {
 								</div>
 							</Dropzone>
 						</div>
-
-						<aside>
-							<ul>
-								{this.state.files.map((f) => (
-									<li key={f.name}>
-										{f.name} - {f.size / 1000} KB
-									</li>
-								))}
-							</ul>
-						</aside>
 					</section>
+					<Input
+						bsSize="sm"
+						className={styles.configText}
+						placeholder="Upload Preset"
+						value={this.state.upload_preset}
+						onChange={this.handleUploadPresetChange}
+					/>
+					<Input
+						bsSize="sm"
+						className={styles.configText}
+						placeholder="API SecretKey"
+						value={this.state.api_key}
+						onChange={this.handleApiKeyChange}
+					/>
+
+					<Input
+						bsSize="sm"
+						className={styles.configText}
+						placeholder="Folder path"
+						value={this.state.folderPath}
+						onChange={this.handleFolderPathChange}
+					/>
 				</div>
 			</div>
 		);
